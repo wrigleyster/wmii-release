075505160bf169fc315ac0d76d842604e6a63d63
jw <jw@altapay.com>
2018-07-06 20:33:49 +0200

    wmii-3.5

--- config.mk
+++ config.mk
@@ -1,4 +1,7 @@
-# Customize to fit your system
+# wmii version
+VERSION = 3.5
+
+# Customize below to fit your system
 
 # paths
 PREFIX = /usr/local
@@ -8,20 +11,22 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-VERSION = 3.1
-
 # includes and libs
-LIBS = -L${PREFIX}/lib -L/usr/lib -lc -L${X11LIB} -lX11
+INCS = -I. -I${PREFIX}/include -I/usr/include -I${X11INC}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -L${PREFIX}/lib -lixp -lm
 
-# Linux/BSD
-CFLAGS = -O3 -I. -I${PREFIX}/include -I/usr/include -I${X11INC} \
-	-DVERSION=\"${VERSION}\"
+# flags
+CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\"
 LDFLAGS = ${LIBS}
+#CFLAGS = -g -Wall -O2 ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = -g ${LIBS}
 
 # Solaris
-#CFLAGS = -fast -xtarget=ultra ${INCLUDES} -DVERSION=\"${VERSION}\"
-#LIBS += -lnsl -lsocket
+#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
+#LDFLAGS = ${LIBS}
+#LDFLAGS += -lsocket -lnsl
+#CFLAGS += -xtarget=ultra
 
-AR = ar cr
+# compiler and linker
 CC = cc
-RANLIB = ranlib
+LD = ${CC}
